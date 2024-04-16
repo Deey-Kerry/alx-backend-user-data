44b41103a1993c1d4b6381663335c2698fc8a0c2
Deey-Kerry <dianahkerrynyash@gmail.com>
2024-04-16 13:43:05 +0000

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python3
-"""Aunthetication module"""
+"""Module of Index views
+"""
 from typing import List, TypeVar
 from flask import request
 
 
 class Auth:
-    """Authorization class"""
+    """Class Authorization"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Aunthetication requirement"""
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        """Requires authorization class"""
+        if path is None:
             return True
-        if path[-1] != "/":
-            path += "/"
 
-        return path not in excluded_paths
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """Authorization header"""
+        """self uthorization header"""
         if request is None:
             return None
-        return request.headers.get("Authorization", None)
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
 
-    def current_user(self, request=None) -> TypeVar("User"):
-        """Current user request"""
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user"""
+
         return None
